9741c5c2d1d8b77ab865087db814c21f858c5c6c
Vitaliy Dobriyan <dobriy88@yandex.ru>
2024-12-21 23:33:14 +0300

    custom activation functions moved to separate classes

---
+++ src/NeuralNetTsa/NeuralNet/CustomActivation/ComplexActivationFunction.cs
@@ -0,0 +1,31 @@
+﻿using ChaosSoft.NeuralNetwork.Activation;
+using NeuralNetTsa.NeuralNet.Entities;
+
+namespace NeuralNetTsa.NeuralNet.CustomActivation;
+
+public abstract class ComplexActivationFunction : IActivationFunction
+{
+    private const int Capacity = 7;
+
+    protected ComplexActivationFunction()
+    {
+        AdditionalNeuron = true;
+
+        Neuron = new InputNeuron(Capacity);
+
+        for (int i = 0; i < Capacity; i++)
+        {
+            Neuron.Outputs.Add(new PruneSynapse(i, i));
+        }
+    }
+
+    public InputNeuron Neuron { get; }
+
+    public bool AdditionalNeuron { get; }
+
+    public abstract string Name { get; }
+
+    public abstract double Dphi(double arg);
+
+    public abstract double Phi(double arg);
+}
